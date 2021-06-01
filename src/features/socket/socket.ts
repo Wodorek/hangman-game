137f3772eb9d569f3f9c27426a7707ef9fccd1df
Wodorek <58212786+Wodorek@users.noreply.github.com>
2021-06-01 17:50:48 +0200

    Update socket.ts

--- src/features/socket/socket.ts
+++ src/features/socket/socket.ts
@@ -4,7 +4,3 @@ const URL = 'http://localhost:3030';
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
-
-socket.onAny((event, ...args) => {
-  console.log(event, args);
-});
